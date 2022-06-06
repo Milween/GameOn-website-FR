4a0cb02a21ef1594359800b99e84dee32d3f8288
Milween <usualksm@gmail.com>
2022-06-06 07:46:02 +0200

    Fermer la modale (X)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,27 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+// Ajout de la fermeture du modal (x)
+const closeModalBtn = document.querySelectorAll("#close");
+
+
+// Events -----------
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
+
+// Functions -----------
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
