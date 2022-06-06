d9d8c08c2d8bfa12c84fbac943aadb1686dc1448
Milween <usualksm@gmail.com>
2022-06-06 08:08:35 +0200

    (tests) Input validaty

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,12 +8,18 @@ function editNav() {
 }
 
 // DOM Elements
+  // Ajout de la fermeture du modal (x)
+  // Ajout du sélecteur form
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-// Ajout de la fermeture du modal (x)
 const closeModalBtn = document.querySelectorAll("#close");
+const formElt = document.querySelectorAll("#form");
 
+// Form Elements 
+const firstElt = document.getElementById("first");
+const lastElt = document.getElementById("last");
+const quantityElt = document.getElementById("quantity");
 
 // Events -----------
 
@@ -24,14 +30,31 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
 
+// Validation du form :
+formElt.forEach(elt => elt.addEventListener("submit, validate"));
+
 // Functions -----------
 
 // launch modal form
+
 function launchModal() {
   modalbg.style.display = "block";
 }
+// close modal form
 
 function closeModal() {
   modalbg.style.display = "none";
 }
+// Validate form :
 
+function validate (valide) {
+  valide.preventDefault();
+  const isFirstValid = isLongEnough(firstElt.value.length, 2) ? true : false;
+  console.log(isFirstValid);
+  const isLastValid = isLongEnough(lastElt.value.length, 2) ? true : false;
+  console.log(isLastValid)
+
+  function isLongEnough(currentLength, minimumLength) {
+    return currentLength >= minimumLength ? true : false;
+  }
+}
