adfb72eb24c926e8bfc3776aee243cd058b57eda
Milween <usualksm@gmail.com>
2022-06-06 08:46:40 +0200

    Test : input validity

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,18 +8,25 @@ function editNav() {
 }
 
 // DOM Elements
-  // Ajout de la fermeture du modal (x)
-  // Ajout du sélecteur form
+  
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+  // Ajout de la fermeture du modal (x)
 const closeModalBtn = document.querySelectorAll("#close");
+  // Ajout du sélecteur form
 const formElt = document.querySelectorAll("#form");
+  // Ajout de la sélection de 'radio'
+const cityElt = document.querySelectorAll('input[type=radio]');
 
 // Form Elements 
 const firstElt = document.getElementById("first");
 const lastElt = document.getElementById("last");
+const emailElt = document.getElementById("email");
 const quantityElt = document.getElementById("quantity");
+const birthdateElt = document.getElementById("birthdate");
+  // Ajout de la sélection de 'checkbox'
+const checkbox1Elt = document.getElementById('checkbox1');
 
 // Events -----------
 
@@ -30,8 +37,11 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
 
-// Validation du form :
+// Validation form :
 formElt.forEach(elt => elt.addEventListener("submit, validate"));
+
+// Ajout changements sur radio inputs
+cityElt.forEach(elt => elt.addEventListener("change", isRadioChecked));
 
 // Functions -----------
 
@@ -45,16 +55,49 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+
 // Validate form :
 
 function validate (valide) {
   valide.preventDefault();
-  const isFirstValid = isLongEnough(firstElt.value.length, 2) ? true : false;
-  console.log(isFirstValid);
-  const isLastValid = isLongEnough(lastElt.value.length, 2) ? true : false;
-  console.log(isLastValid)
+  
+  const isFirstValid = isLongEnough(firstElt.value.length, 2);  
+  const isLastValid = isLongEnough(lastElt.value.length, 2);
+  const isEmailValid = isStringMatchEmailFormat(emailElt.value);
+  console.log(isEmailValid);
+  const isQuantityValid = isFilledWithNumber(quantityElt.value);
+  console.log(isQuantityValid);
+  const isCityValid = isRadioChecked();
+  console.log(isCityValid);
+  const isConditionsValid = isCheckboxChecked("checkbox1");
+  console.log(isConditionsValid);
+}
 
-  function isLongEnough(currentLength, minimumLength) {
-    return currentLength >= minimumLength ? true : false;
-  }
+// vérifier si le format Email est correct 
+function isStringMatchEmailFormat(str) {
+  const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return mailFormat.test(str);
+}
+
+// Vérifier si c'est bien un nombre ou vide
+function isFilledWithNumber(data) {
+  return data != "" && !isNaN(data) ? true : false;
+}
+
+// Vérifier si une pastille ville est coché 
+function isRadioChecked() {
+  return document.querySelectorAll("input[type=radio]:checked").length > 0;
+}
+
+function isCheckboxChecked(id) {
+  return document.getElementById(id).checked;
+}
+
+
+
+
+// Vérifier si la nombre de caractères est respecté :
+function isLongEnough(currentLength, minimumLength) {
+  return currentLength >= minimumLength ? true : false;
 }
